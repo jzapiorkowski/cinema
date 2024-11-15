31bb582d4e31af2b8e8badb35589eeec8e3cf860
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2024-11-15 13:59:26 +0100

    feat: serialize enums to string

--- server/Cinema.API/Startup.cs
+++ server/Cinema.API/Startup.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Cinema.API.Core.Converters;
 using Cinema.API.Core.Middlewares;
 using Cinema.API.Core.SchemaFilters;
@@ -24,7 +25,11 @@ public class Startup
         services.AddControllers(
                 options => { options.SuppressAsyncSuffixInActionNames = false; }
             )
-            .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new TimeSpanConverter()); });
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new TimeSpanConverter());
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
     }
 
     public static void ConfigureMiddleware(WebApplication app, IWebHostEnvironment env)
